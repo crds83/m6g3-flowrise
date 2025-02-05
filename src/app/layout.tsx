db38ba47391a5bdd124ce59b552edc860a93e2d5
crds83 <119341646+crds83@users.noreply.github.com>
2025-02-05 19:26:59 +0000

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { repositoryName } from "@/prismicio";
 import clsx from "clsx";
 import { createClient } from "@/prismicio";
 
+import Header from "@/components/Header";
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -24,8 +25,8 @@ export async function generateMetadata(): Promise<Metadata> {
   const page = await client.getSingle("settings");
  
   return {
-    title: page.data.site_title || "Artsphere",
-    description: page.data.meta_description || "Artshere is a great app for artists",
+    title: settings.data.site_title || "Artsphere",
+    description: settings.data.meta_description || "Artshere is a great app for artists",
     openGraph: {
       images: [page.data.og_images.url || ""],
     },
@@ -41,10 +42,10 @@ export default function RootLayout({
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
 
-        <header>Header!</header>
+        <Header />
         {children}
         <PrismicPreview repositoryName={repositoryName} />
-        <footer>Footer!</footer>
+        
       </body>
     </html>
   );
