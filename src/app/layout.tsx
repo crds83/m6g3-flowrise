e57c9fb93c30b6fe14a0cad2fc2e9bbac75bcc3d
crds83 <119341646+crds83@users.noreply.github.com>
2025-02-05 13:54:26 +0000

    feat:Added Fonts and cleared boiler plate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import clsx from "clsx";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
+  display: 'swap'
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
   subsets: ["latin"],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -27,10 +30,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={clsx(nunito.variable, nunitoSans.variable)}> 
         {children}
-        <PrismicPreview repositoryName={repositoryName} />
+        
       </body>
     </html>
   );
