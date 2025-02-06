41cd017a2be6dbf300907dc767b9719b8192d48b
crds83 <119341646+crds83@users.noreply.github.com>
2025-02-06 12:11:42 +0000

    sfs

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,14 +1,22 @@
-"use client";
-
-import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
+import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../../slices";
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
-export default function SliceSimulatorPage() {
-  return (
-    <SliceSimulator
-      sliceZone={(props) => <SliceZone {...props} components={components} />}
-    />
-  );
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
