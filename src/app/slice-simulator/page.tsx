72a09332ca20c2ce398f153a2c1df3ea113e60e5
crds83 <119341646+crds83@users.noreply.github.com>
2025-02-06 12:51:27 +0000

    afafa

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,22 +1,14 @@
-import { Metadata } from "next";
+"use client";
+
+import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
 
-import { createClient } from "@/prismicio";
-import { components } from "@/slices";
+import { components } from "../../slices";
 
-export default async function Page() {
-  const client = createClient();
-  const page = await client.getSingle("homepage");
-
-  return <SliceZone slices={page.data.slices} components={components} />;
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  const client = createClient();
-  const page = await client.getSingle("homepage");
-
-  return {
-    title: page.data.meta_title,
-    description: page.data.meta_description,
-  };
+export default function SliceSimulatorPage() {
+  return (
+    <SliceSimulator
+      sliceZone={(props) => <SliceZone {...props} components={components} />}
+    />
+  );
 }
