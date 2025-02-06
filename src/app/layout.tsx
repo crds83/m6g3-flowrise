7b0c1b011aefeedaf4630c72c9956775e09cf85a
crds83 <119341646+crds83@users.noreply.github.com>
2025-02-06 11:57:15 +0000

    photos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,53 @@
-import type { Metadata, ResolvingMetadata } from "next";
-import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import type { Metadata } from "next";
 import clsx from "clsx";
-import { createClient } from "@/prismicio";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import { createClient, repositoryName } from "@/prismicio";
+import { PrismicPreview } from "@prismicio/next";
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const nunito = Nunito({
-  variable: "--font-nunito",
   subsets: ["latin"],
-  display: 'swap'
+  display: "swap",
+  variable: "--font-nunito",
 });
 
 const nunitoSans = Nunito_Sans({
-  variable: "--font-nunito-sans",
   subsets: ["latin"],
-  display: 'swap',
+  display: "swap",
+  variable: "--font-nunito-sans",
 });
 
 export async function generateMetadata(): Promise<Metadata> {
-  const client = createClient(); 
+  const client = createClient();
+
   const settings = await client.getSingle("settings");
- 
+
   return {
-    title: settings.data.site_title || "Artsphere",
-    description: settings.data.meta_description || "Artshere is a great app for artists",
+    title: settings.data.site_title || "Flowrise",
+    description:
+      settings.data.meta_description || "Flowrise is the relaxing app for you.",
     openGraph: {
-      images: [page.data.og_images.url || ""],
+      images: [settings.data.og_image.url || ""],
     },
-  }
+  };
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
-
-        
+        <Header />
         {children}
+        <Footer />
+        <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50" />
         <PrismicPreview repositoryName={repositoryName} />
-        
       </body>
     </html>
   );
--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,21 +1,14 @@
-import {
-  SliceSimulator,
-  SliceSimulatorParams,
-  getSlices,
-} from "@slicemachine/adapter-next/simulator";
+"use client";
+
+import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
 
 import { components } from "../../slices";
 
-export default async function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams) {
-  const { state } = await searchParams;
-  const slices = getSlices(state);
-
+export default function SliceSimulatorPage() {
   return (
-    <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
-    </SliceSimulator>
+    <SliceSimulator
+      sliceZone={(props) => <SliceZone {...props} components={components} />}
+    />
   );
 }
